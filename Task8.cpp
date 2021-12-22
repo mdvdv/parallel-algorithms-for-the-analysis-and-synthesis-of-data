dba2ddff551d6dc192dfbf8e70aabf24b18ae165
mdvdv <anatolmdvdv@gmail.com>
2021-12-22 21:31:15 +0300

    Updated main

--- Task8.cpp
+++ Task8.cpp
@@ -3,62 +3,46 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
+int main(int argc, char* argv[])
 {
-    int size, rank;
-    double start_time, time;
-    int length = 1;
-    int count, array[1000000];
-    
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
-    MPI_Status status;
-    
-    if (rank == 0)
-    {
-	while (length < 1000000)
-	{
-    	    start_time = MPI_Wtime();
-	    for (int i = 0; i < 10000; i++)
-	    {
-		MPI_Send(&array, length, MPI_INT, 1, 5, MPI_COMM_WORLD);
-		MPI_Recv(&array, length, MPI_INT, 1, 10, MPI_COMM_WORLD, &status);
-	    }
-	    
-	    time = MPI_Wtime() - start_time;
-	    cout << "Message Size: " << length * 0.000001 * sizeof(int) << endl;
-	    length *= 10;
-	}
-	
-	start_time = MPI_Wtime();
-	for (int i = 0; i < 10000; i++)
-	{
-	    MPI_Send(&array, 0, MPI_INT, 1, 5, MPI_COMM_WORLD);
-	    MPI_Recv(&array, 0, MPI_INT, 1, 10, MPI_COMM_WORLD, &status);
-	}
-	
-	time = MPI_Wtime() - start_time;
-	cout << "Latency: " << time / 2 << endl;
-    	MPI_Send(&rank, 1, MPI_INT, 1, 15, MPI_COMM_WORLD);
-    }
-    
-    if (rank == 1)
-    {
-	while (true)
-	{
-	    MPI_Probe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-	    if (status.MPI_TAG == 15)
-	    {
-		break;
-	    }
-	    
-	    MPI_Get_count(&status, MPI_INT, &count);
-	    MPI_Recv(&array, count, MPI_INT, 0, 5, MPI_COMM_WORLD, &status);
-	    MPI_Send(&array, count, MPI_INT, 0, 10, MPI_COMM_WORLD);
-	}
-    }
+    int rank, size;
 
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Status status;
+
+    char *message;
+    const int max_length = 1000000;
+
+    for (int length = 1; length <= max_length; length *= 10)
+    {
+	message = new char[length];
+	double start_time = MPI_Wtime();
+	for (int i = 0; i < 10; i++)
+	{
+	    if (rank == 0)
+	    {
+		MPI_Send(message, length, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+		MPI_Recv(message, length, MPI_CHAR, 1, 1, MPI_COMM_WORLD, &status);
+	    }
+
+	    if (rank == 1)
+    	    {
+		MPI_Recv(message, length, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+		MPI_Send(message, length, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+	    }
+	}
+	
+	if (rank == 0)
+	{
+	    double stop = MPI_Wtime();
+	    double time = stop - start_time;
+	    double bandwidth = 2. * 10 * length / time;
+	    double latency = time / (2. * 10);
+
+	    cout << "Latency: " << latency << endl;
+	    cout << "Message Size: " << length << " Bandwidth: " << bandwidth / time << " bytes/s" << endl;
+	}
+    }
     MPI_Finalize();
 }
